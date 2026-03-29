7dde87f90cf820858800751cb7474ccc40de95ff
chrispam123 <uopechris@gmail.com>
2026-03-29 15:41:53 +0200

    infra: setup remote state with s3 native locking

---
+++ infra/main.tf
@@ -0,0 +1,49 @@
+terraform {
+  required_version = ">= 1.11.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  # EL BLOQUE BACKEND VA AQUÍ DENTRO
+  backend "s3" {
+    bucket       = "nocturne-tfstate-chrispam-2026"
+    key          = "global/s3/terraform.tfstate"
+    region       = "eu-west-1"
+    encrypt      = true
+    use_lockfile = true 
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+# --- RECURSOS DEL BUCKET (Ya deberían estar creados) ---
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "nocturne-tfstate-chrispam-2026"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_versioning" "enabled" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
+  bucket = aws_s3_bucket.terraform_state.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
